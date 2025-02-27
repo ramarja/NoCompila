9bc2631c9b9d6de5aeb5a83aa8ce2e373d8361af
1coolname <jobaro3@alumni.uv.es>
2025-02-27 10:24:01 +0100

    hola

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 #include "ThisThread.h"
 #include "mbed.h"
 #include "InterruptIn.h"
+#include "plt_iks4a1.h"
 
 InterruptIn int1(BUTTON1);
 DigitalOut myled(LED1);
+LPS22DF sensor(I2C_SDA, I2C_SCL); 
 int delay =1000;
+
 
 void pressed()
 {
@@ -27,12 +30,16 @@ int main() //Hola
 
     int measure[5];
     float averageValue;
-
+float pressure, temperature; 
     int1.rise(&pressed);
+    sensor.begin();
+    sensor.Enable(); 
 
     while (true) {
 
-
+        sensor.GetPressure(&pressure);
+        sensor.GetTemperature(&temperature);
+        printf("Pressure[hPa]: %f , Temperature[C]: %f\r\n", pressure, temperature);
         myled = !myled;
         ThisThread::sleep_for(delay);
 
